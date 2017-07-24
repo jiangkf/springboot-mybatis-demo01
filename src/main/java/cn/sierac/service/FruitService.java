e8c368b428d5ef892501a0c271caa1028481a883
Jack <674877849@qq.com>
2017-07-24 14:55:24 +0800

    修改

--- src/main/java/cn/sierac/service/FruitService.java
+++ src/main/java/cn/sierac/service/FruitService.java
@@ -19,13 +19,18 @@ public class FruitService {
     public List<Fruit> getAll(){
         return  fruitMapperDao.getAll();
     }
+
     public Fruit get(Integer id){
         return  fruitMapperDao.get(id);
     }
+
     public List<Fruit> getByShop(Integer sid){return fruitMapperDao.getByShop(sid);}
+
     public void insertFruit(Fruit fruit){fruitMapperDao.insert(fruit);}
+
     public void updateFruit(Fruit fruit){fruitMapperDao.update(fruit);}
-    public void deleteFruit(int id){fruitMapperDao.delete(id);}
+
+    public void deleteFruit(Integer id){fruitMapperDao.delete(id);}
 
 
 
