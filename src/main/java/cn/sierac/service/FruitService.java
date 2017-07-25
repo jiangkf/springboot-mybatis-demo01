f2492acc9579c7f10ff22d3253d8eed261cf855d
Jack <674877849@qq.com>
2017-07-25 14:20:22 +0800

    commits

--- src/main/java/cn/sierac/service/FruitService.java
+++ src/main/java/cn/sierac/service/FruitService.java
@@ -4,6 +4,7 @@ import cn.sierac.entity.Fruit;
 import cn.sierac.mapper.FruitMapperDao;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -16,20 +17,26 @@ public class FruitService {
     @Autowired
     private FruitMapperDao fruitMapperDao;
 
+    @Transactional(readOnly = true)
     public List<Fruit> getAll(){
         return  fruitMapperDao.getAll();
     }
 
+    @Transactional(readOnly = true)
     public Fruit get(Integer id){
         return  fruitMapperDao.get(id);
     }
 
+    @Transactional(readOnly = false)
     public List<Fruit> getByShop(Integer sid){return fruitMapperDao.getByShop(sid);}
 
+    @Transactional(readOnly = false)
     public void insertFruit(Fruit fruit){fruitMapperDao.insert(fruit);}
 
+    @Transactional(readOnly = false)
     public void updateFruit(Fruit fruit){fruitMapperDao.update(fruit);}
 
+    @Transactional(readOnly = false)
     public void deleteFruit(Integer id){fruitMapperDao.delete(id);}
 
 
